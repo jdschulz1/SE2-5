f30dbaac9b037d9a3aa05f704fc2f19228123e66
Jennifer Foster <jennifer.foster@eagles.oc.edu>
2017-06-04 19:57:39 -0500

    Added Menu Bar and Edit Company Information window

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import views.CompanyInfoEditView;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 
 
@@ -14,6 +20,25 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("ACME Couriers Delivery Tracker");
+			
+			MenuBar menuBar = new MenuBar();
+	        Menu menuMaintenance = new Menu("Maintainance");
+	        MenuItem menuEditCompanyInformation = new MenuItem("Edit Company Information");
+	        menuEditCompanyInformation.setOnAction(
+	        		new EventHandler<ActionEvent>() {
+	        			public void handle(ActionEvent t) {
+	        				CompanyInfoEditView view = new CompanyInfoEditView();
+	  
+	            }});
+	        menuMaintenance.getItems().add(menuEditCompanyInformation);
+	        Menu menuDeliveryTracker = new Menu("Delivery Tracker");
+	        Menu menuReports = new Menu("Reports");
+	        Menu menuAccount = new Menu("Account");
+	 
+	        menuBar.getMenus().addAll(menuMaintenance, menuDeliveryTracker, menuReports, menuAccount);
+	        root.setTop(menuBar);
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
---
+++ src/views/CompanyInfoEditView.java
@@ -0,0 +1,35 @@
+package views;
+
+import java.text.DecimalFormat;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class CompanyInfoEditView {
+
+	public CompanyInfoEditView () {
+		
+		DecimalFormat df; 
+		df = new DecimalFormat("$#,##0.00");
+		
+		Stage stage = new Stage();
+		stage.setTitle("Edit Company Information");
+		BorderPane root = new BorderPane();
+		
+		Scene scene = new Scene(root, 300, 200);
+		stage.setScene(scene);
+		
+	    Button saveButton = new Button("Save");
+//		saveButton.setOnAction(e -> { 
+//		
+//		});
+		saveButton.setPrefWidth(scene.getWidth());
+		saveButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		root.setBottom(saveButton);
+			
+		stage.show();
+	}
+	
+}
