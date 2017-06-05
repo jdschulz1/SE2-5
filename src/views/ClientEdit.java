df4db972382e1bac31bd3ca8d546bf1eec6c9d6d
Jennifer Foster <jennifer.foster@eagles.oc.edu>
2017-06-05 10:58:42 -0500

    Added Menu and Text Fields to Company Edit Info screen, added Client Info fxml, working on switching between scenes

---
+++ src/views/ClientEdit.java
@@ -0,0 +1,12 @@
+package views;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientEdit implements javafx.fxml.Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		System.out.println("Client Edit");
+	}
+}
