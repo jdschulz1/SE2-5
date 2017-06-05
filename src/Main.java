07b53f760bc299d565413bf6ac69e73105b43c31
Jennifer Foster <jennifer.foster@eagles.oc.edu>
2017-06-05 10:57:54 -0500

    Added Menu to Company Edit Info screen

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,24 @@
 
 	
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.stage.Stage;
-import views.CompanyInfoEditView;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage stage) {
+	public void start(Stage primaryStage) {
 		Parent root = null;
 		try {
 			root = FXMLLoader.load(getClass().getClassLoader().getResource("views/CompanyInfoEditView.fxml"));
+//			root = FXMLLoader.load(getClass().getClassLoader().getResource("views/ClientEdit.fxml"));
+
 			Scene scene = new Scene(root);
-			stage.setScene(scene);
-			stage.sizeToScene();
-			stage.show();
+			primaryStage.setScene(scene);
+			primaryStage.sizeToScene();
+			primaryStage.show();
 //			Parent root = FXMLLoader.load(getClass().getResource("HelloWorld.fxml"));
 //		    
 //	        Scene scene = new Scene(root, 300, 275);
--- src/views/CompanyInfoEditView.java
+++ src/views/CompanyInfoEditView.java
@@ -1,90 +1,50 @@
 package views;
 
-import java.io.IOException;
-import java.text.DecimalFormat;
-
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class CompanyInfoEditView implements javafx.fxml.Initializable {
 
-	//public CompanyInfoEditView () {
-		
-	//    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CompanyInfoEditView.fxml"));
-	//    fxmlLoader.setRoot(this);
-	//    fxmlLoader.setController(this);
-	    
-	//    try {
-			//Parent root = FXMLLoader.load(getClass().getResource("HelloWorld.fxml"));
-
-	//        fxmlLoader.load();            
-	//    } catch (IOException exception) {
-	//        throw new RuntimeException(exception);
-	//    }
-	//}
 	@FXML 
-	private Button btnHello;
+	private Button btnSave;
+	@FXML 
+	private Button btnCancel;
+	@FXML 
+	private TextField textFieldCompanyName;
+	@FXML 
+	private TextField textFieldStreet;
+	
 	@Override	
 	public void initialize(URL location, ResourceBundle resources) {		
-		//btnHello.setOnAction(new EventHandler() {
-		//    @Override public void handle(ActionEvent e) {
-		    	//txfHello.setText("Hello!");
-		//    }
-	//	});		
+		btnSave.setOnAction(new EventHandler() {
+		   @Override
+			public void handle(Event event) {
+				textFieldCompanyName.setText("ACME");
+				try {
+					Parent parent = FXMLLoader.load(getClass().getClassLoader().getResource("views/ClientEdit.fxml"));
+					Scene parentScene = new Scene(parent);
+					Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+					stage.setScene(parentScene);
+					stage.show();
+				}
+				catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});		
 	}
 }
-		
-		
-//		Parent root = FXMLLoader.load(getClass().getResource("HelloWorld.fxml"));
-//	    
-//        Scene scene = new Scene(root, 300, 275);
-//		Stage stage = new Stage();
-//        stage.setTitle("FXML Welcome");
-//        stage.setScene(scene);
-//        stage.show();
-		
-//		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
-//				"HelloWorld.fxml"));
-//				        fxmlLoader.setRoot(this);
-//				        fxmlLoader.setController(this);
-//
-//				        try {
-//				            fxmlLoader.load();
-//				        } catch (IOException exception) {
-//				            throw new RuntimeException(exception);
-//				        }
-//				    }
-		
-//		DecimalFormat df; 
-//		df = new DecimalFormat("$#,##0.00");
-//		
-//		Stage stage = new Stage();
-//		stage.setTitle("Edit Company Information");
-//		BorderPane root = new BorderPane();
-//		
-//		Scene scene = new Scene(root, 300, 200);
-//		stage.setScene(scene);
-//		
-//	    Button saveButton = new Button("Save");
-//	    		
-//		saveButton.setPrefWidth(scene.getWidth());
-//		saveButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-//		root.setBottom(saveButton);
-//			
-//		stage.show();
-//	}
-	
-//}
