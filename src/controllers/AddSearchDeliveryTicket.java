ba32b5d1bce6b35a8a242764c52db92c4f474560
Anna Mercer <Anna Mercer@JohnLaptop>
2017-06-15 05:30:47 -0500

    Starting on Add/Search

--- src/controllers/AddSearchDeliveryTicket.java
+++ src/controllers/AddSearchDeliveryTicket.java
@@ -2,25 +2,90 @@ package controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.util.StringConverter;
+import model.Client;
+import model.DeliveryTicket;
+import model.DeliveryTracker;
 
 public class AddSearchDeliveryTicket implements Initializable {
 
     @FXML
     private Button buttonNewDeliveryTicket;
     
+    @FXML
+    private ComboBox<Client> comboBoxClient;
+    
+    @FXML
+    private DatePicker datePickerSearchDate;
+
+    @FXML
+    private TableColumn<DeliveryTicket, String> tableColumnClient;
+
+    @FXML
+    private TableColumn<DeliveryTicket, String> tableColumnCourier;
+
+    @FXML
+    private TableColumn<DeliveryTicket, String> tableColumnOrderDate;
+
+    @FXML
+    private TableColumn<DeliveryTicket, String> tableColumnPackageID;
+   
+    @FXML
+    private Button btnDeliveryTicketUpdate;
+
+    @FXML
+    private Button btnDeliveryTicketCancel;
+    
+
+    @FXML
+    private Button buttonSearchDeliveryTickets;
+    
+    DeliveryTracker deliveryTracker;
+    List<DeliveryTicket> tickets;
+    DateTimeFormatter formatter;
+    
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+		deliveryTracker = DeliveryTracker.getDeliveryTracker();
+		updateClientsList();
+		
+		comboBoxClient.setConverter(
+		            new StringConverter<Client>() {
+		                @Override
+		                public Client fromString(String s) {
+	                    	//TODO: get client by name
+	                        return null;
+		                }
+
+						@Override
+						public String toString(Client object) {
+							if (object == null) {
+		                        return "";
+		                    } else {
+		                        return object.getName();
+		                    }
+						}
+		 });
+			
 		buttonNewDeliveryTicket.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -37,6 +102,23 @@ public class AddSearchDeliveryTicket implements Initializable {
     	    	}
             }
         });
+		
+		buttonSearchDeliveryTickets.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+              
+            }
+        });
+		
 	}
 
+
+
+private void updateClientsList() {
+	
+	ObservableList<Client> clients = FXCollections.observableArrayList();
+	clients.addAll(deliveryTracker.getClients());
+	comboBoxClient.setItems(clients);
+	}
+	
 }
